c065da27d7d919f931f96b358b9d72207d45368f
Andy Ghiuta <andy.ghiuta@gmail.com>
2018-08-02 18:17:37 +0300

    Chat example

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const drawingRoutes = require('./routes/drawing.js');
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 function test() {
   console.log(123);
@@ -16,4 +18,23 @@ app.get('/test', (req, res) => {
   test();
   res.json({ ok: true });
 });
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// websocket connection
+io.on('connection', (socket) => {
+  console.log('user connected');
+  let myUsername;
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat:connect', ({ username }) => {
+    myUsername = username;
+    console.log('username: ' + username);
+    io.emit('chat:connected', username);
+  });
+  socket.on('chat:message', ({ message }) => {
+    console.log('message: ' + message);
+    io.emit('chat:messaged', { username: myUsername, message });
+  });
+});
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'));
---
+++ public/src/chat.js
@@ -0,0 +1,39 @@
+const socket = io('http://localhost:3000', {
+  transports: ['websocket'],
+  upgrade: false,
+  autoConnect: false,
+});
+const chatContainer = document.getElementById('chat');
+const addMessageToList = function addMessageToList(message) {
+  const li = document.createElement('li');
+  li.innerHTML = message;
+  chatContainer.appendChild(li);
+};
+socket.on('chat:connected', (username) => {
+  addMessageToList(`<i>${username}</i> connected`);
+});
+socket.on('chat:messaged', ({ username, message }) => {
+  addMessageToList(`<i>${username}</i>: ${message}`);
+});
+
+const connectBtn = document.getElementById('connect');
+const sendBtn = document.getElementById('send');
+const usernameInput = document.getElementById('username');
+const messageInput = document.getElementById('message');
+
+connectBtn.addEventListener('click', () => {
+  socket.open();
+  socket.emit('chat:connect', {
+    username: usernameInput.value
+  });
+  connectBtn.setAttribute('disabled', 'disabled');
+  usernameInput.setAttribute('disabled', 'disabled');
+  sendBtn.removeAttribute('disabled');
+  messageInput.removeAttribute('disabled');
+}, false);
+
+sendBtn.addEventListener('click', () => {
+  socket.emit('chat:message', {
+    message: messageInput.value
+  });
+});
